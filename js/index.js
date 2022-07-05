1b4e91b693bde7842781f2cf150b024f80612744
keke <97692860+Chirpingdusty@users.noreply.github.com>
2022-07-05 10:06:18 -0700

    playRound function added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-function ask() {
-  console.log("werweare")
+let arr = ["paper", "scissors", "rock"]
+function computerPlay() {
+  return arr[Math.floor(Math.random() * 3)]
 }
+
+function playRound(playerChoice, computerChoice) {
+  if (computerChoice === "paper") {
+    console.log('win')
+  } else if (computerChoice === "scissors") {
+    console.log('lose')
+  } else {
+    console.log('play again')
+  }
+}
+
+const playerChoice = "rock";
+const computerChoice = computerPlay();
+
+console.log(playRound(playerChoice, computerChoice))
